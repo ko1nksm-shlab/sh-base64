a10a04a517d342d0ec2bd2cfe923ebf021f81f34
Koichi Nakashima <koichi@nksm.name>
2023-08-15 17:07:49 +0900

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # sh-base64
+
+Base64 encoder/decoder implementation for portable shell scripts
+
+## Requirements
+
+- POSIX shell (dash, bash, ksh, mksh, yash, zsh, etc)
+- Basic POSIX commands (`od`, `tr`, `fold`, `xargs`, and `awk`)
+
+## Usage
+
+```sh
+. ./base64.sh
+
+# Base64 encode
+base64encode       # Basic usage (base64 standard)
+base64encode "+/"  # Use + and / for the 62nd and 63rd characters, no padding
+base64encode "-_"  # Use - and _ for the 62nd and 63rd characters, no padding (base64url)
+base64encode "+/=" # Use + and / for the 62nd and 63rd characters and = for padding
+
+# Base64 decode
+base64decode       # Basic usage (base64 standard)
+base64decode "+/"  # Use + and / for the 62nd and 63rd characters
+base64decode "-_"  # Use - and _ for the 62nd and 63rd characters (base64url)
+```
+
+## Example
+
+```console
+$ echo abc | ./base64
+YWJjCg==
+
+$ echo abc | ./base64 | ./base64 -d
+abc
+```
---
+++ base64.sh
@@ -0,0 +1,61 @@
+# shellcheck shell=sh
+
+base64encode() {
+  set -- "${1:-"+/="}"
+  set -- "$@" "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  od -v -An -tx1 | LC_ALL=C tr -d ' \n' | LC_ALL=C fold -b -w6 | {
+    LC_ALL=C awk -v x="$2${1%=}" -v p="${1#??}" '
+      function dec2bin(n, w,  r) {
+        r = ""
+        do { r = (n % 2) r } while ( n = int(n / 2) )
+        return sprintf("%0" w "d", r)
+      }
+      BEGIN {
+        for (i = 0; i < 256; i++) b[sprintf("%02x", i)] = dec2bin(i, 8)
+        for (i = 0; i < 64; i++) c[dec2bin(i, 6)] = substr(x, i + 1, 1)
+      }
+      {
+        pad = 3 - (length / 2); bits = chars = ""
+        for (i = 0; i < pad; i++) $0 = $0 "00"
+        for (i = 1; i <= 6; i+=2) bits = bits b[substr($0, i, 2)]
+        for (i = 1; i <= 24; i+=6) chars = chars c[substr(bits, i, 6)]
+        if (pad > 0) chars = substr(chars, 1, 4 - pad)
+        while (pad--) chars = chars p
+        printf "%s", chars
+      }
+      END { print "" }
+    '
+  }
+}
+
+base64decode() {
+  set -- "${1:-"+/="}"
+  set -- "$@" "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  LC_ALL=C fold -b -w100 | { # fold width must be a multiple of 4
+    LC_ALL=C awk -v x="$2${1%=}" -v p="${1#??}" '
+      function dec2bin(n, w,  r) {
+        r = ""
+        do { r = (n % 2) r } while ( n = int(n / 2) )
+        return sprintf("%0" w "d", r)
+      }
+      BEGIN {
+        for (i = 0; i < 64; i++) b[substr(x, i + 1, 1)] = dec2bin(i, 6)
+        for (i = 1; i < 256; i++) c[dec2bin(i, 8)] = sprintf("%c", i)
+        for (i = 48; i < 56; i++) c[dec2bin(i, 8)] = sprintf("\\\\%03o", i)
+        c["00000000"] = "\\\\000"; c["00001001"] = "\\\\011"
+        c["00001010"] = "\\\\012"; c["00100000"] = "\\\\040"
+        c["00100010"] = "\\\\042"; c["00100101"] = "\\\\045"
+        c["00100111"] = "\\\\047"; c["01011100"] = "\\\\\\\\"
+      }
+      {
+        bits = chars = ""; len = length
+        for (i = 1; i <= len; i++) bits = bits b[substr($0, i, 1)]
+        for (i = 1; i <= len * 6; i+=8) chars = chars c[substr(bits, i, 8)]
+        print chars
+      }
+    '
+  } | (
+    set -- '[ ${KSH_VERSION:+x} ] && alias printf="print -n";'
+    xargs -n 1000 sh -c "$1"'IFS=; printf -- "$*"' "${ZSH_ARGZERO:-"$0"}"
+  )
+}
